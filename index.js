4ccaa138180354510e48c6e22d498c20de1f9753
rajaazan69 <azanr7260@gmail.com>
2025-07-15 05:25:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ client.on('interactionCreate', async interaction => {
   .setTimestamp();
 
         await ticket.send({
-  content: `<@${interaction.user.id}> <@${OWNER_ID}>`,
+  content: `**Welcome To Azan Middleman Services**\nYou Will Be Assisted Shortly\n\n<@${interaction.user.id}> <@${OWNER_ID}>`,
   embeds: [embed]
 });
           
