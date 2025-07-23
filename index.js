5367b761516bf9a87245459822e18789bc68941b
rajaazan69 <azanr7260@gmail.com>
2025-07-23 21:14:04 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ client.once('ready', () => {
   console.log(`✅ Bot is ready: ${client.user.tag}`);
 });
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
   .then(() => console.log("✅ Login successful"))
   .catch((err) => console.error("❌ Login failed:", err));
