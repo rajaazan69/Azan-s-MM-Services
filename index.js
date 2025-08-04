dd76a9ba9ee44d8b27146768f493f6f939fc4299
rajaazan69 <azanr7260@gmail.com>
2025-08-04 08:25:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -776,11 +776,11 @@ const ticket = await interaction.guild.channels.create({
     `**User 1:** <@${interaction.user.id}>\n` +
     `**User 2:** ${targetMention}\n\n` +
     `**Trade Details**\n` +
-    `> ${q1}\n\n` +
+    `> ${`q1`}\n\n` +
     `**User 1 is giving:**\n` +
-    '```' + q2 + '```\n\n' +
+    '```' + `q2` + '```\n\n' +
     `**User 2 is giving:**\n` +
-    '```' + q3 + '```'
+    '```' + `q3` + '```'
   )
   .setFooter({ text: `Ticket by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() })
   .setTimestamp();
