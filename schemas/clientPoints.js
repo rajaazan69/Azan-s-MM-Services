fb6365ab3b11f8e9d2f5fccbb346bb5478244c4f
rajaazan69 <azanr7260@gmail.com>
2025-08-05 22:01:56 +0500

    Delete schemas directory

--- schemas/clientPoints.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const clientPointsSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
-  points: { type: Number, default: 0 }
-});
-
-module.exports = mongoose.model('ClientPoints', clientPointsSchema);
